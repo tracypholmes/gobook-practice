46fcfe42dac0def603ff1b0a8ef75381777e34f7
Tracy Holmes <tracypholmes@gmail.com>
2018-11-14 15:45:47 -0600

    my 1st program :raised_hands:

---
+++ chapter2/main.go
@@ -0,0 +1,10 @@
+package main // package declaration. Every go program must start with this
+
+import "fmt"
+
+// this is a comment
+
+func main() {
+	x := "Tracy"
+	fmt.Println("Well, Hello World! My name is " + x)
+}
